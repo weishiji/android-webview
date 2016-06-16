f040ba26f3375d228934079338ef4fcd30de672b
lxg <lxg1986@gmail.com>
2016-06-16 20:46:39 +0800

    bug fixed

--- app/src/main/java/com/eliyet/yang/light/demo/lightdemo/MainActivity.java
+++ app/src/main/java/com/eliyet/yang/light/demo/lightdemo/MainActivity.java
@@ -1,10 +1,14 @@
 package com.eliyet.yang.light.demo.lightdemo;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.Window;
 import android.webkit.ClientCertRequest;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -12,31 +16,54 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
     private String myUrl = "https://www.stylewe.com";
+    final Activity activity = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
         initWebView();
+
         mWebView.loadUrl(myUrl);
     }
 
     @SuppressLint("JavascriptInterface")
     private void initWebView() {
+        //Add Progress bar Support
+        this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
+
+        setContentView(R.layout.activity_main);
+
+        // Makes Progress bar Visible
+        this.getWindow().setFeatureInt( Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
+
+        // Get Web view
         mWebView = (WebView) findViewById(R.id.activity_main_wv);
-        mWebView.setWebViewClient(new WebViewClient() {
+
+        //to the WebView in the main.xml
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setSupportZoom(true);       //Zoom Control on web (You don't need this
+        //if ROM supports Multi-Touch
+        mWebView.getSettings().setBuiltInZoomControls(true); //Enab
+
+
+        mWebView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
-
+        });
+        mWebView.setWebChromeClient(new WebChromeClient(){
             @Override
-            public void onPageFinished(WebView view, String url) {
-                MainActivity.this.setTitle(view.getTitle());
+            public void onReceivedTitle(WebView view, String title) {
+                activity.setTitle(title);
+            }
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                activity.setProgress(newProgress * 1000);
             }
         });
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+
 //        mWebView.addJavascriptInterface(new Object() {
 //            public void clickOnAndroid() {
 //            }
